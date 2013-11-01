f2102fd61cfa5bce897854fa0111bddd5bc0fbc7
fish <CaviarWang@gmail.com>
2013-11-01 16:26:29 +0800

    no class path

--- src/com/example/whattoeat/MainActivity.java
+++ src/com/example/whattoeat/MainActivity.java
@@ -2,16 +2,30 @@ package com.example.whattoeat;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	int i,j;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		//testtttt
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	
+	
+	public void toEatBtOnclick(View aView)
+	{
+		Intent intent = new Intent();
+		intent.setClass(MainActivity.this, RestauruntActivity.class);
+		startActivity(intent); 
+		MainActivity.this.finish(); 
+	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
