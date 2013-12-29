f46be7120473fd78a9bff701c657dc152ddbe7f3
CaviarWang <CaviarWang@gmail.com>
2013-12-30 05:29:06 +0800

    pagerView

--- gen/com/example/whattoeat/R.java
+++ gen/com/example/whattoeat/R.java
@@ -36,7 +36,7 @@ public final class R {
         public static final int ImageView03=0x7f08000e;
         public static final int ImageView04=0x7f08000f;
         public static final int LinearLayout1=0x7f08000a;
-        public static final int action_settings=0x7f080012;
+        public static final int action_settings=0x7f080014;
         public static final int backbtn=0x7f080005;
         public static final int editText1=0x7f080004;
         public static final int goimg=0x7f080009;
@@ -45,21 +45,29 @@ public final class R {
         public static final int imageView1=0x7f08000b;
         public static final int imageView2=0x7f08000c;
         public static final int itemTitle=0x7f080000;
-        public static final int menu_settings=0x7f080011;
+        public static final int menu_settings=0x7f080013;
         public static final int newPhotobtn=0x7f080001;
         public static final int optionbtn=0x7f080006;
         public static final int ratingBar1=0x7f080003;
+        public static final int textView1=0x7f080011;
         public static final int view1=0x7f080002;
+        public static final int viewpagerLayout=0x7f080012;
     }
     public static final class layout {
         public static final int activity_item=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int activity_restaurunt=0x7f030002;
+        public static final int activity_pager=0x7f030002;
+        public static final int activity_restaurunt=0x7f030003;
+        public static final int layout1=0x7f030004;
+        public static final int layout2=0x7f030005;
+        public static final int layout3=0x7f030006;
+        public static final int viewpager_layout=0x7f030007;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
-        public static final int item=0x7f070001;
-        public static final int restaurunt=0x7f070002;
+        public static final int activity_pager=0x7f070001;
+        public static final int item=0x7f070002;
+        public static final int restaurunt=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050004;
@@ -67,6 +75,7 @@ public final class R {
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
         public static final int title_activity_item=0x7f050005;
+        public static final int title_activity_pager=0x7f050006;
         public static final int title_activity_restaurunt=0x7f050003;
     }
     public static final class style {
